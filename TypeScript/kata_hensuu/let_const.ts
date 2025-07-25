dffad6c89337c169c4f1194cc10cc60d4c47ae1a
ika-2-2 <219417869+ika-2-2@users.noreply.github.com>
2025-07-26 02:29:46 +0900

    let,constの使い方を理解した。

---
+++ TypeScript/kata_hensuu/let_const.ts
@@ -0,0 +1,19 @@
+//変数提言
+
+// letは再代入が可能な変数提言
+let a = 1;
+a = 2;
+
+//constは再代入が不可能な変数提言
+const b = "Hello World";
+b = "Hello TypeScript";  //←これは無理
+
+//しかし、オブジェクトの変更は可能
+const obj = {a: 1};
+obj = {a: 2};   //再代入は無理、
+obj.a = 2;      //これはOK（プロパティの変更）
+
+const list = [1, 2, 3];
+list = [2, 3, 4]; //再代入は無理、
+list[0] = 1;   //要素の変更はOK
+list.push(3); //これも要素の変更だからOK
